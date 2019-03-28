81422cb4b17530f9cda61c5fc3819866fd9cc86a
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-03-28 11:29:23 +0100

    FIX Removes unused config in projects.cljs
    - Adds description, etc.
    - Remove all old lein-figwheel config (we use figwheel-main)
    - Uses firefox for default :doo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kti-web "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "FIXME: A frontend for KTI"
+  :url "https://github.com/vitorqb/kti-web/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -26,16 +26,13 @@
             [lein-asset-minifier "0.2.7"
              :exclusions [org.clojure/clojure]]]
 
-  :ring {:handler kti-web.handler/app
-         :uberwar-name "kti-web.war"}
-
+  :ring {:handler kti-web.handler/app :uberwar-name "kti-web.war"}
   :min-lein-version "2.5.0"
   :uberjar-name "kti-web.jar"
   :main kti-web.server
-  :clean-targets ^{:protect false}
-  [:target-path
-   [:cljsbuild :builds :app :compiler :output-dir]
-   [:cljsbuild :builds :app :compiler :output-to]]
+  :clean-targets ^{:protect false} [:target-path
+                                    [:cljsbuild :builds :app :compiler :output-dir]
+                                    [:cljsbuild :builds :app :compiler :output-to]]
 
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :resource-paths ["resources" "target/cljsbuild"]
@@ -56,7 +53,6 @@
               :pretty-print  false}}
             :app
             {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-             :figwheel {:on-jsload "kti-web.core/mount-root"}
              :compiler
              {:main "kti-web.dev"
               :asset-path "/js/out"
@@ -72,27 +68,9 @@
                         :output-to "target/test.js"
                         :output-dir "target/cljstest/public/js/out"
                         :optimizations :whitespace
-                        :pretty-print true}}
+                        :pretty-print true}}}}
 
-
-            }
-   }
-   :doo {:build "test"
-         :alias {:default [:chrome]}}
-
-  :figwheel
-  {:http-server-root "public"
-   :server-port 3449
-   :nrepl-port 7002
-   :nrepl-middleware [cider.piggieback/wrap-cljs-repl
-                      cider.nrepl/cider-middleware
-                      refactor-nrepl.middleware/wrap-refactor
-                      ]
-   :css-dirs ["resources/public/css"]
-   :ring-handler kti-web.handler/app}
-
-
-
+  :doo {:build "test" :alias {:default [:firefox]}}
   :profiles {:dev {:repl-options {:init-ns kti-web.repl}
                    :dependencies [[cider/piggieback "0.4.0"]
                                   [binaryage/devtools "0.9.10"]
@@ -102,25 +80,17 @@
                                   [figwheel-sidecar "0.5.18"]
                                   [nrepl "0.6.0"]
                                   [pjstadig/humane-test-output "0.9.0"]
-                                  [com.bhauman/figwheel-main "0.2.1-SNAPSHOT"]
-                                  
- ]
-
+                                  [com.bhauman/figwheel-main "0.2.1-SNAPSHOT"]]
                    :source-paths ["env/dev/clj" "env/dev/cljs"]
-                   :plugins [[lein-figwheel "0.5.18"]
-                             [lein-doo "0.1.10"]
+                   :plugins [[lein-doo "0.1.10"]
                              [cider/cider-nrepl "0.21.0"]
                              [org.clojure/tools.namespace "0.3.0-alpha4"
                               :exclusions [org.clojure/tools.reader]]
                              [refactor-nrepl "2.4.0"
-                              :exclusions [org.clojure/clojure]]
-]
-
+                              :exclusions [org.clojure/clojure]]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
-
                    :env {:dev true}}
-
              :uberjar {:hooks [minify-assets.plugin/hooks]
                        :source-paths ["env/prod/clj"]
                        :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
