e42ec65e346ba4e3656707830082ea0c2557c038
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-05-01 14:35:17 +0200

    OPT Uses new patterns on `edit-captured-reference-component`

--- src/cljs/kti_web/components/edit_captured_reference_component.cljs
+++ src/cljs/kti_web/components/edit_captured_reference_component.cljs
@@ -2,33 +2,24 @@
   (:require
    [cljs.core.async :refer [go <! >! timeout]]
    [reagent.core :as r]
-   [kti-web.components.utils :refer [submit-button call-prevent-default]]
+   [kti-web.components.utils
+    :refer [submit-button call-prevent-default make-input]
+    :as components-utils]
    [kti-web.components.select-captured-ref :refer [select-captured-ref]]
    [kti-web.utils :refer [call-prevent-default call-with-val to-str]]
-   [kti-web.utilsc :refer-macros [go-with-done-chan]]
-))
+   [kti-web.utilsc :refer-macros [go-with-done-chan]]))
 
-(defn captured-ref-inputs--id [{:keys [id]}]
-  [:div [:span "Id"] [:input {:value (or id "") :disabled true}]])
-
-(defn captured-ref-inputs--created-at [{:keys [created-at]}]
-  [:div
-   [:span "Captured at"]
-   [:input {:value (or created-at "") :disabled true}]])
-
-(defn captured-ref-inputs--reference [{:keys [reference on-reference-change]}]
-  [:div 
-   [:span "Reference"]
-   [:input {:value (or reference "")
-            :on-change (call-with-val on-reference-change)}]])
+(def inputs
+  {:id         (make-input {:text "Id" :perm-disabled true :type "number"})
+   :created-at (make-input {:text "Created at" :perm-disabled true})
+   :reference  (make-input {:text "Reference"})})
 
 (defn captured-ref-inputs [{:keys [value on-change]}]
   [:div
-   [captured-ref-inputs--id value]
-   [captured-ref-inputs--created-at value]
-   [captured-ref-inputs--reference
-    {:reference (:reference value)
-     :on-reference-change #(on-change (assoc value :reference %))}]])
+   [(:id inputs)         {:value (:id value)}]
+   [(:created-at inputs) {:value (:created-at value)}]
+   [(:reference inputs)  {:value (:reference value)
+                          :on-change #(on-change (assoc value :reference %))}]])
 
 (defn edit-captured-ref-form
   [{:keys [cap-ref on-cap-ref-change on-submit]}]
@@ -38,60 +29,66 @@
    [submit-button]])
 
 (defn edit-captured-ref-comp--inner
-  [{:keys [get-captured-ref on-cap-ref-selection cap-ref-id-value
-           cap-ref-selection-error on-cap-ref-id-change editted-cap-ref
-           on-editted-cap-ref-change on-edit-cap-ref-submit loading?
-           toggle-loading status]}]
+  [{:keys [get-captured-ref on-cap-ref-selection selected-cap-ref-id-value
+           on-cap-ref-id-change editted-cap-ref on-editted-cap-ref-change
+           on-edit-cap-ref-submit loading? toggle-loading]
+    :as props}]
   [:div
    [:h3 "Edit Captured Reference Form"]
    [select-captured-ref
     {:get-captured-ref get-captured-ref
      :on-selection on-cap-ref-selection
-     :id-value cap-ref-id-value
+     :id-value selected-cap-ref-id-value
      :on-id-change on-cap-ref-id-change
      :toggle-loading toggle-loading}]
    (cond
-     loading? [:span "Loading..."]
-     cap-ref-selection-error [:span (str "ERROR: " cap-ref-selection-error)]
+     loading?
+     [:span "Loading..."]
+     (get-in props [:status :select-cap-ref :errors])
+     [components-utils/errors-displayer
+      {:status (get-in props [:status :select-cap-ref])}]
      editted-cap-ref
-     [edit-captured-ref-form
-      {:cap-ref editted-cap-ref
-       :on-cap-ref-change on-editted-cap-ref-change
-       :on-submit on-edit-cap-ref-submit}])
-   [:div status]])
+     [:div
+      [edit-captured-ref-form
+       {:cap-ref editted-cap-ref
+        :on-cap-ref-change on-editted-cap-ref-change
+        :on-submit on-edit-cap-ref-submit}]
+      [components-utils/errors-displayer
+       {:status (get-in props [:status :edit-cap-ref])}]
+      [components-utils/success-message-displayer
+       {:status (get-in props [:status :edit-cap-ref])}]])])
 
 (defn edit-captured-ref-comp [{:keys [hput! hget!]}]
   "A form to edit a captured reference."
-  (let [selected-id-value (r/atom nil)
-        cap-ref-selection-error (r/atom nil)
-        editted-cap-ref (r/atom nil)
-        status (r/atom nil)
-        loading? (r/atom false)
+  (let [state (r/atom {:selected-cap-ref-id-value nil
+                       :editted-cap-ref nil
+                       :loading? false
+                       :status {:edit-cap-ref {} :select-cap-ref {}}})
         handle-submit
         (fn []
-          (let [resp-chan (hput! @selected-id-value @editted-cap-ref)]
+          (swap! state assoc
+                 :loading? true
+                 :status {:edit-cap-ref {} :select-cap-ref {}})
+          (let [{:keys [selected-cap-ref-id-value editted-cap-ref]} @state
+                resp-chan (hput! selected-cap-ref-id-value editted-cap-ref)]
             (go-with-done-chan
              (let [{:keys [error? data]} (<! resp-chan)]
-               (reset! status (if error?
-                                (str "Error: " (to-str data))
-                                "Success!"))))))
+               (swap! state assoc
+                      :loading? false
+                      :status {:edit-cap-ref (if error?
+                                               {:errors data}
+                                               {:success-msg "Success!"})})))))
         handle-cap-ref-selection
         (fn [{:keys [error? data]}]
-          (reset! editted-cap-ref nil)
-          (reset! cap-ref-selection-error nil)
-          (if error?
-            (reset! cap-ref-selection-error (to-str data))
-            (reset! editted-cap-ref data)))]
+          (swap! state assoc
+                 :status {:select-cap-ref (and error? {:errors data})}
+                 :editted-cap-ref (or error? data)))]
     (fn []
       [edit-captured-ref-comp--inner
-       {:get-captured-ref hget!
-        :on-cap-ref-selection handle-cap-ref-selection
-        :cap-ref-selection-error @cap-ref-selection-error
-        :cap-ref-id-value @selected-id-value
-        :on-cap-ref-id-change #(reset! selected-id-value %)
-        :editted-cap-ref @editted-cap-ref
-        :on-editted-cap-ref-change #(reset! editted-cap-ref %)
-        :on-edit-cap-ref-submit handle-submit
-        :status @status
-        :loading? @loading?
-        :toggle-loading #(reset! loading? %)}])))
+       (assoc @state
+              :get-captured-ref hget!
+              :on-cap-ref-selection handle-cap-ref-selection
+              :on-cap-ref-id-change #(swap! state assoc :selected-cap-ref-id-value %)
+              :on-editted-cap-ref-change #(swap! state assoc :editted-cap-ref %)
+              :on-edit-cap-ref-submit handle-submit
+              :toggle-loading #(swap! state assoc :loading? %))])))
--- test/cljs/kti_web/components/edit_captured_reference_component_test.cljs
+++ test/cljs/kti_web/components/edit_captured_reference_component_test.cljs
@@ -1,63 +1,34 @@
 (ns kti-web.components.edit-captured-reference-component-test
   (:require
    [cljs.test :refer-macros [is are deftest testing use-fixtures async]]
-   [cljs.core.async :refer [chan <! >! put! go]]
+   [cljs.core.async
+    :refer [chan <! >! put! go]
+    :as async]
    [reagent.core :as reagent :refer [atom]]
    [kti-web.http :as http]
    [kti-web.components.edit-captured-reference-component :as rc]
    [kti-web.components.select-captured-ref :refer [select-captured-ref]]
+   [kti-web.components.utils :as components-utils]
    [kti-web.utils :refer [to-str]]
    [kti-web.test-utils :as utils]
    [kti-web.test-factories :as factories]))
 
-(deftest test-captured-ref-inputs--id
-  (let [mount rc/captured-ref-inputs--id
-        get-value #(get-in % [2 1 :value])
-        get-disabled #(get-in % [2 1 :disabled])]
-    (testing "Is disabled"
-      (is (true? (get-disabled (mount)))))
-    (testing "Shows value from id"
-      (is (= (get-value (mount {:id 12})) 12)))))
-
-(deftest test-captured-ref-inputs--created-at
-  (let [mount rc/captured-ref-inputs--created-at
-        get-value #(get-in % [2 1 :value])
-        get-disabled #(get-in % [2 1 :disabled])]
-    (testing "Is disabled"
-      (is (true? (get-disabled (mount)))))
-    (testing "Shows value from created-at"
-      (is (= (get-value (mount {:created-at :bar})) :bar)))))
-
-(deftest test-captured-ref-inputs--reference
-  (let [mount rc/captured-ref-inputs--reference
-        get-value #(get-in % [2 1 :value])
-        get-on-change #(get-in % [2 1 :on-change])]
-    (testing "Shows reference in value"
-      (is (= (get-value (mount {:reference :foo})) :foo)))
-    (testing "Calls on-reference-change"
-      (let [[on-change-args on-change] (utils/args-saver)
-            comp (mount {:on-reference-change on-change})]
-        ((get-on-change comp) (clj->js {:target {:value "foo"}}))
-        (is (= @on-change-args [["foo"]]))))))
-
 (deftest test-captured-ref-inputs
   (let [mount rc/captured-ref-inputs]
-    (testing "Passes value to reference input"
-      (let [comp (mount {:value {:reference :foo}})]
-        (is (= (get-in comp [3 0]) rc/captured-ref-inputs--reference))
-        (is (= (get-in comp [3 1 :reference]) :foo))))
-    (testing "Calls on-change if reference changes"
-      (let [[on-change-args on-change] (utils/args-saver)
-            value {:id 99 :reference "foo"}
-            comp (mount {:value value :on-change on-change})]
-        ((get-in comp [3 1 :on-reference-change]) "New Ref")
-        (is (= [[{:id 99 :reference "New Ref"}]] @on-change-args))))
-    (testing "Renders id input"
-      (let [value {:foo "bar"} comp (rc/captured-ref-inputs {:value value})]
-        (is (= (get-in comp [1]) [rc/captured-ref-inputs--id value]))))
-    (testing "Renders created-at input"
-      (let [value {:bar :baz} comp (rc/captured-ref-inputs {:value value})]
-        (is (= (get-in comp [2]) [rc/captured-ref-inputs--created-at value]))))))
+    (let [comp (mount {:value {:id ::id
+                               :created-at ::created-at
+                               :reference ::reference}})]
+      (testing "Renders id input"
+        (is (= (get-in comp [1 1 :value]) ::id)))
+      (testing "Renders created-at input"
+        (is (= (get-in comp [2 1 :value]) ::created-at)))
+      (testing "Renders reference input"
+        (is (= (get-in comp [3 1 :value]) ::reference))))
+    (testing "Calls on-change for reference"
+      (let [[args fun] (utils/args-saver)
+            comp (mount {:value {:id ::id} :on-change fun})]
+        ((get-in comp [3 1 :on-change]) ::reference)
+        (is (= @args [[{:id ::id :reference ::reference}]]))))))
 
 (deftest test-edit-captured-ref-form
   (let [mount rc/edit-captured-ref-form]
@@ -76,11 +47,13 @@
 
 (deftest test-captured-ref-comp--inner
   (let [mount rc/edit-captured-ref-comp--inner
-        get-captured-ref-form #(get-in % [3])]
+        get-captured-ref-form #(get-in % [3 1])
+        get-error-component #(get-in % [3 2])
+        get-success-msg-component #(get-in % [3 3])]
     (testing "Initializes select-captured-ref"
       (let [params {:get-captured-ref :a
                     :on-cap-ref-selection :b
-                    :cap-ref-id-value :c
+                    :selected-cap-ref-id-value :c
                     :on-cap-ref-id-change :d
                     :toggle-loading :e}]
         (is (= (get-in (mount params) [2])
@@ -93,13 +66,11 @@
       (let [params {:editted-cap-ref :a
                     :on-editted-cap-ref-change :b
                     :on-edit-cap-ref-submit :c
-                    :status :d}]
+                    :edit-cap-ref-status :d}]
         (is (= (get-captured-ref-form (mount params))
                [rc/edit-captured-ref-form {:cap-ref :a
                                            :on-cap-ref-change :b
                                            :on-submit :c}]))))
-    (testing "Shows status"
-      (is (= (get-in (mount {:status "FOO"}) [4]) [:div "FOO"])))
     (testing "Hides edit-captured-ref-form if editted-cap-ref is nil"
       (is (nil? (get-captured-ref-form (mount {}))))
       (is (nil? (get-captured-ref-form (mount {:editted-cap-ref nil}))))
@@ -113,9 +84,19 @@
         ((get-in comp [2 1 :toggle-loading]) true)
         ((get-in comp [2 1 :toggle-loading]) false)
         (is (= @toggle-loading-args [[true] [false]]))))
-    (testing "Shows error msg if cap-ref-selection-error is set"
-      (is (= (get-in (mount {:cap-ref-selection-error "foo"}) [3])
-             [:span "ERROR: foo"])))))
+    (testing "Displayes errors for select-cap-ref"
+      (is (= (get-in (mount {:status {:select-cap-ref {:errors "foo"}}}) [3])
+             [components-utils/errors-displayer {:status {:errors "foo"}}])))
+    (testing "Displays error msg"
+      (let [errors-map {:errors "foo"}
+            comp (mount {:status {:edit-cap-ref errors-map} :editted-cap-ref {}})]
+        (is (= (get-error-component comp)
+               [components-utils/errors-displayer {:status errors-map}]))))
+    (testing "Displayers success-msg"
+      (let [success-map {:success-msg "bar"}
+            comp (mount {:status {:edit-cap-ref success-map} :editted-cap-ref {}})]
+        (is (= (get-success-msg-component comp)
+               [components-utils/success-message-displayer {:status success-map}]))))))
 
 (deftest test-edit-captured-ref-comp
   (let [get-inner-prop (fn [c k] (get-in c (conj [1] k)))]
@@ -123,21 +104,21 @@
       (let [comp ((rc/edit-captured-ref-comp {:hget! :a}))]
         (is (= (get-in comp [0]) rc/edit-captured-ref-comp--inner))
         (is (= (get-inner-prop comp :get-captured-ref) :a))
+        (is (= (get-inner-prop comp :status) {:edit-cap-ref {} :select-cap-ref {}}))
         (is (not (nil? (get-inner-prop comp :on-cap-ref-selection))))
-        (is (nil? (get-inner-prop comp :cap-ref-id-value)))
-        (is (nil? (get-inner-prop comp :cap-ref-selection-error)))
+        (is (nil? (get-inner-prop comp :selected-cap-ref-id-value)))
         (is (not (nil? (get-inner-prop comp :on-cap-ref-id-change))))
         (is (nil? (get-inner-prop comp :editted-cap-ref)))
         (is (not (nil? (get-inner-prop comp :on-editted-cap-ref-change))))
         (is (not (nil? (get-inner-prop comp :on-edit-cap-ref-submit))))
         (is (not (nil? (get-inner-prop comp :toggle-loading))))
-        (is (nil? (get-inner-prop comp :status)))
+        (is (nil? (get-inner-prop comp :edit-cap-ref-status)))
         (is (false? (get-inner-prop comp :loading?)))))
-    (testing "Updates cap-ref-id-value on cap-ref-id-change"
+    (testing "Updates selected-cap-ref-id-value on cap-ref-id-change"
       (let [comp-1 (rc/edit-captured-ref-comp {})]
-        (is (nil? (get-inner-prop (comp-1) :cap-ref-id-value)))
+        (is (nil? (get-inner-prop (comp-1) :selected-cap-ref-id-value)))
         ((get-inner-prop (comp-1) :on-cap-ref-id-change) 999)
-        (is (= (get-inner-prop (comp-1) :cap-ref-id-value) 999))))
+        (is (= (get-inner-prop (comp-1) :selected-cap-ref-id-value) 999))))
     (testing "Updates editted-cap-ref on editted-cap-ref-change"
       (let [comp-1 (rc/edit-captured-ref-comp {})]
         (is (nil? (get-inner-prop (comp-1) :editted-cap-ref)))
@@ -154,20 +135,21 @@
         (is (true? (get-inner-prop (comp-1) :loading?)))
         ((get-inner-prop (comp-1) :toggle-loading) false)
         (is (false? (get-inner-prop (comp-1) :loading?)))))
-    (testing "Updated cap-ref-selection-error if selection fails"
+    (testing "Updated status errors if selection fails"
       (let [comp-1 (rc/edit-captured-ref-comp {})
             ;; The http response has an error
             http-response factories/http-response-error-msg]
         ;; Calls on-cap-ref-selection with an errored response
         ((get-inner-prop (comp-1) :on-cap-ref-selection)
          (http/parse-response http-response))
-        ;; Expects cap-ref-selection-error to have been set
-        (is (= (get-inner-prop (comp-1) :cap-ref-selection-error)
-               (to-str {:ROOT (get-in http-response [:body :error-msg])})))
+        ;; Expects status to be set
+        (is (= (get-inner-prop (comp-1) :status)
+               {:select-cap-ref
+                {:errors (:data (http/parse-response http-response))}}))
         ;; Now simulates a selection that works just fine
-        ((get-inner-prop (comp-1) :on-cap-ref-selection) {})
+        ((get-inner-prop (comp-1) :on-cap-ref-selection) {::data {::a ::b}})
         ;; And the error should be nil again
-        (is (nil? (get-inner-prop (comp-1) :cap-ref-selection-error)))))))
+        (is (= (get-inner-prop (comp-1) :status) {:select-cap-ref nil}))))))
 
 (deftest test-edit-captured-ref-comp--calls-hput-on-submit
   (let [[hput!-args update-hput!-args] (utils/args-saver)
@@ -186,14 +168,20 @@
     ((get-in (comp-1) [1 :on-editted-cap-ref-change]) new-cap-ref)
     ;; And submits
     (let [ret-chan ((get-in (comp-1) [1 :on-edit-cap-ref-submit]))]
+      ;; We see it loading
+      (is (true? (get-in (comp-1) [1 :loading?])))
       ;; hput! was called with the id and the new cap ref
       (is (= @hput!-args [[(:id cap-ref) new-cap-ref]]))
-      ;; hput! writes something into the channel and the status is Success!
       (async done
              (go
+               ;; hput! writes something into the channel
                (>! hput!-chan 1)
                (is (= (<! ret-chan) :done))
-               (is (= (get-in (comp-1) [1 :status]) "Success!"))
+               ;; We see it loading not more
+               (is (false? (get-in (comp-1) [1 :loading?])))
+               ;; The success msg is set
+               (is (= (get-in (comp-1) [1 :status :edit-cap-ref])
+                      {:success-msg "Success!"}))
                (done))))))
 
 (deftest test-edit-captured-ref-comp--sets-error
@@ -214,7 +202,33 @@
                ;; An error is returned
                (>! hput!-chan parsed-error)
                (is (= (<! ret-chan) :done))
-               ;; And the status is the error
-               (is (= (get-in (comp-1) [1 :status])
-                      (str "Error: " (to-str (:data parsed-error)))))
+               ;; And the error shows up in status
+               (is (= (get-in (comp-1) [1 :status :edit-cap-ref])
+                      {:errors (parsed-error :data)}))
                (done))))))
+
+(deftest test-edit-captured-ref-comp--resets-state-between-submits
+  (let [http-error {:error? true :data {::a ::b}}
+        http-success {:data {::c ::d}}
+        hput!-chan (async/timeout 2000)
+        comp1 (rc/edit-captured-ref-comp {:hput! (constantly hput!-chan)})]
+    ;; User submits
+    (let [ret-chan ((get-in (comp1) [1 :on-edit-cap-ref-submit]))]
+      (async
+       done
+       (go
+         ;; An error is returned
+         (>! hput!-chan http-error)
+         (is (= (<! ret-chan) :done))
+         ;; And the error is seen
+         (is (= (get-in (comp1) [1 :status :edit-cap-ref])
+                {:errors {::a ::b}}))
+         ;; He submits again
+         (let [ret-chan ((get-in (comp1) [1 :on-edit-cap-ref-submit]))]
+           ;; A success msg is returned
+           (>! hput!-chan http-success)
+           (is (= (<! ret-chan) :done))
+           ;; and the status is set
+           (is (= (get-in (comp1) [1 :status :edit-cap-ref])
+                  {:success-msg "Success!"}))
+           (done)))))))
