69c3ea9f9038798b45705cbc0265648ce310eef8
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-05-11 17:50:17 +0200

    Version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kti-web "0.3.0"
+(defproject kti-web "0.4.0"
   :description "FIXME: A frontend for KTI"
   :url "https://github.com/vitorqb/kti-web/"
   :license {:name "Eclipse Public License"
