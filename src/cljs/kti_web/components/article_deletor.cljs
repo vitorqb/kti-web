a916e09967081c5edcadeb207e68fb59c5127c60
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-07-07 15:24:51 +0200

    OPT Uses plain functions as handlers for `article-deletor`

--- src/cljs/kti_web/components/article_deletor.cljs
+++ src/cljs/kti_web/components/article_deletor.cljs
@@ -5,60 +5,64 @@
    [kti-web.utils :as utils]
    [kti-web.components.utils :as components-utils :refer [input]]))
 
-(defprotocol ArticleDeletorEvents
-  "Events for article deletor"
-  (on-delete-article-id-submit [this]
-    "Handles confirmation of deletion for the article.")
-
-  (on-delete-article-id-change [this new-value]
-    "Handles a chanve of value for the id to be deleted"))
-
-(defn make-success-msg [id] (str "Deleted article with id " id))
-
-(defn reduce-before-article-deletion [state] (assoc state :status {} :loading? true))
+;; Helpers
+(defn make-success-msg [id]
+  (str "Deleted article with id " id))
 
 (defn ->status [state {:keys [error? data]}]
   (if error?
     {:errors data}
     {:success-msg (make-success-msg (:delete-article-id state))}))
 
+;; Reducers
+(defn reduce-before-article-deletion [state]
+  (assoc state :status {} :loading? true))
+
 (defn reduce-after-article-deletion
   "Prepares state after article deletion, parsing the response."
   [state http-resp]
   (assoc state :loading? false :status (->status state http-resp)))
 
-(defn new-handler [state {:keys [delete-article!]}]
-  (reify ArticleDeletorEvents
+(defn reduce-on-delete-article-id-change [state new-value]
+  {:pre [(and new-value (number? new-value))]}
+  (assoc state :delete-article-id new-value))
 
-    (on-delete-article-id-submit [_]
-      (let [{:keys [delete-article-id]} @state]
-        (swap! state reduce-before-article-deletion)
-        (let [resp-chan (delete-article! delete-article-id)]
-          (go (swap! state reduce-after-article-deletion (<! resp-chan))
-              :done))))
+;; Handlers
+(defn handle-on-delete-article-id-submit [state {:keys [delete-article!]}]
+  (fn []
+    (let [{:keys [delete-article-id]} @state]
+      (swap! state reduce-before-article-deletion)
+      (let [resp-chan (delete-article! delete-article-id)]
+        (go (swap! state reduce-after-article-deletion (<! resp-chan)))))))
 
-    (on-delete-article-id-change [_ new-value]
-      (swap! state assoc :delete-article-id new-value))))
+(defn handle-on-delete-article-id-change [state _]
+  (fn [new-value]
+    (swap! state reduce-on-delete-article-id-change new-value)))
 
+;; State
 (def initial-state
   {:delete-article-id nil
    :status {:errors nil :success-msg nil}
    :loading? false})
 
+;; Components
 (defn article-deletor--inner
   "Pure component for article deletion"
-  [{:keys [delete-article-id handler loading?] :as specs}]
+  [{:keys [delete-article-id
+           on-delete-article-id-submit
+           on-delete-article-id-change
+           loading?]
+    :as specs}]
   [:div {}
    [:h4 "Delete Article"]
    (if loading?
      [:div "Loading..."]
-     [:form {:on-submit (utils/call-prevent-default
-                         #(on-delete-article-id-submit handler))}
+     [:form {:on-submit (utils/call-prevent-default #(on-delete-article-id-submit))}
       [input
        {:text "Article Id: "
         :width 100
         :value delete-article-id
-        :on-change #(on-delete-article-id-change handler %)}]
+        :on-change #(on-delete-article-id-change %)}]
       [components-utils/submit-button]])
    [components-utils/errors-displayer specs]
    [components-utils/success-message-displayer specs]])
@@ -66,7 +70,11 @@
 (defn article-deletor
   "Component and state manager for article deletor"
   [{:keys [delete-article!] :as props}]
-  (let [state (atom initial-state) handler (new-handler state props)]
+  (let [state (atom initial-state)]
     (fn []
       [article-deletor--inner
-       (merge props @state {:handler handler})])))
+       (merge
+        props
+        @state
+        {:on-delete-article-id-change (handle-on-delete-article-id-change state props)
+         :on-delete-article-id-submit (handle-on-delete-article-id-submit state props)})])))
--- test/cljs/kti_web/components/article_deletor_test.cljs
+++ test/cljs/kti_web/components/article_deletor_test.cljs
@@ -6,18 +6,6 @@
    [kti-web.components.utils :as components-utils]
    [kti-web.test-utils :as utils]))
 
-(defn new-test-handler [calls-atom]
-  (let [save-call! (fn [kw args] (swap! calls-atom update kw #(conj % args)))]
-    (reify rc/ArticleDeletorEvents
-
-      (on-delete-article-id-submit [_]
-        (save-call! :on-delete-article-id-submit []))
-
-      (on-delete-article-id-change [_ new-value]
-        (save-call! :on-delete-article-id-change [new-value])))))
-
-(def handler-calls (atom {}))
-(def test-handler (new-test-handler handler-calls))
 (def example-state {:delete-article-id ::a
                     :loading? ::b
                     :status {:errors ::c :success-msg ::d}})
@@ -44,13 +32,15 @@
           :status {:errors {::a ::b}}
           :loading? false)))))
 
-(deftest test-new-handler--on-delete-article-id-change
-  (let [state (atom {})
-        handler (rc/new-handler state {:delete-article-id ::foo})]
-    (rc/on-delete-article-id-change handler ::bar)
-    (is (= @state {:delete-article-id ::bar}))))
+(deftest test-handle-on-delete-article-id-change
+  (let [[args saver] (utils/args-saver)
+        state (atom {})
+        props {:delete-article-id ::foo}
+        handler (rc/handle-on-delete-article-id-change state props)]
+    (handler 999)
+    (is (= @state {:delete-article-id 999}))))
 
-(deftest test-new-handler--on-delete-article-id-submit
+(deftest test--on-delete-article-id-submit
   (let [http-resp {}
         delete-article-chan (async/to-chan http-resp)
         [args saver] (utils/args-saver)
@@ -61,18 +51,18 @@
         initial-state {:delete-article-id delete-article-id}
         state (atom initial-state)
         props {:delete-article! delete-article!}
-        handler (rc/new-handler state props)]
+        handler (rc/handle-on-delete-article-id-submit state props)]
     (async
      done
      (go
-       (let [resp-chan (rc/on-delete-article-id-submit handler)]
+       (let [resp-chan (handler)]
          ;; State is reduced
          (is (= @state (rc/reduce-before-article-deletion initial-state)))
          ;; Called delete with id
          (is (= @args [[[delete-article-id]]]))
          ;; Response arrives
          (>! delete-article-chan {})
-         (is (= (<! resp-chan) :done)))
+         (<! resp-chan))
        ;; State is reduced
        (is (= @state
               (-> initial-state
@@ -84,20 +74,19 @@
   (let [mount rc/article-deletor--inner]
 
     (testing "Renders input for delete-article-id"
-      (reset! handler-calls {})
-      (let [comp (mount {:delete-article-id ::id :handler test-handler})]
+      (let [[args saver] (utils/args-saver)
+            comp (mount {:delete-article-id ::id
+                         :on-delete-article-id-change saver})]
         (is (= (get-in comp [3 2 0]) components-utils/input))
         (is (= (get-in comp [3 2 1 :value]) ::id))
         ((get-in comp [3 2 1 :on-change]) ::foo)
-        (is (= (@handler-calls :on-delete-article-id-change)
-               [[::foo]]))))
+        (is (= @args [[::foo]]))))
 
     (testing "On submit"
-      (reset! handler-calls {})
-      (let [comp (mount {:handler test-handler})]
+      (let [[args saver] (utils/args-saver)
+            comp (mount {:on-delete-article-id-submit saver})]
         ((get-in comp [3 1 :on-submit]) (utils/prevent-default-event))
-        (is (= (@handler-calls :on-delete-article-id-submit)
-               [[]]))))
+        (is (= @args [[]]))))
 
     (testing "Display errors"
       (let [status {:status {:errors {::a ::b}}}
@@ -118,25 +107,30 @@
   (let [mount rc/article-deletor]
 
     (testing "Mounts inner component with correct args"
-      (with-redefs [rc/new-handler (constantly ::new-handler)]
+      (with-redefs [rc/handle-on-delete-article-id-change
+                    (constantly ::on-delete-article-id-change)
+                    rc/handle-on-delete-article-id-submit
+                    (constantly ::on-delete-article-id-submit)]
         (let [comp ((mount)) props (get comp 1)]
           (is (= (get-in comp [0]) rc/article-deletor--inner))
           (are [k v] (and (contains? props k) (= (get props k) v))
             :delete-article-id nil
             :status {:errors nil :success-msg nil}
             :loading? false
-            :handler ::new-handler))))))
+            :on-delete-article-id-change ::on-delete-article-id-change
+            :on-delete-article-id-submit ::on-delete-article-id-submit))))))
 
 (deftest test-article-deletor-success-submit
   (let [[delete-article!-args save-delete-article!-args] (utils/args-saver)
         delete-chan (async/timeout 3000)
         delete-article! #(do (save-delete-article!-args %) delete-chan)
         comp1 (rc/article-deletor {:delete-article! delete-article!})
-        handler (get-in (comp1) [1 :handler])]
+        on-delete-article-id-change (get-in (comp1) [1 :on-delete-article-id-change])
+        on-delete-article-id-submit (get-in (comp1) [1 :on-delete-article-id-submit])]
     ;; User selects an id
-    (rc/on-delete-article-id-change handler ::id)
+    (on-delete-article-id-change 9999)
     ;; And submits
-    (let [out-chan (rc/on-delete-article-id-submit handler)]
+    (let [out-chan (on-delete-article-id-submit)]
       (async
        done
        (go
@@ -144,10 +138,10 @@
          (is (true? (get-in (comp1) [1 :loading?])))
          ;; And the response arrives
          (>! delete-chan {:data ::data})
-         (is (= (<! out-chan) :done))
+         (<! out-chan)
          ;; The component is no longer loading
          (is (not (get-in (comp1) [1 :loading?])))
          ;; The deletion was a success
          (is (= (get-in (comp1) [1 :status])
-                {:success-msg (rc/make-success-msg ::id)}))
+                {:success-msg (rc/make-success-msg 9999)}))
          (done))))))
